4a4446253cf3a0d33b4459291449e8bb465ae6f3
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-05-27 15:48:48 +0900

    맨 처음 페이지 opacity 0으로 변경하는 소스 추가

---
+++ main.js
@@ -0,0 +1,8 @@
+const cover = document.querySelector(".cover");
+const coverBtn = document.querySelector(".cover-btn");
+
+coverBtn.addEventListener("click", removeCover);
+
+function removeCover() {
+  cover.style.opacity = "0";
+}
